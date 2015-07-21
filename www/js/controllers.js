dc2286d73f6b0d8471b9cd5dc681c870634e27e4
Jeff Pelletier <jeff.pelletier@intridea.com>
2015-07-21 11:10:25 -0400

    Inject $cordovaContacts service into AccountCtrl controller.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,7 +21,7 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $cordovaContacts) {
   $scope.settings = {
     enableFriends: true
   };
